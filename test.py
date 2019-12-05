ce000f0ae5b35b83da697af1d0d777611389388c
mengda <30865838+daysunshine@users.noreply.github.com>
2019-12-05 15:29:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,23 +6,5 @@
 # #
 # import bcrypt
 #
-# a = '$2b$12$EjkudLxAsQtFJLGWwI6vFOSl117cfFoj8/vBxeg/cVqmqPXXwYLzC'
-#
-# b = bcrypt.checkpw('12345678'.encode(), a.encode())
-# print(b)
 
-# cc = bcrypt.hashpw('12345678'.encode(), bcrypt.gensalt())
-# print(cc)
-
-
-# lists = HostInfo.objects.filter(id=1)
-#
-# print(lists)
-
-
-a = {'1':'asda','sa':123}
-
-a.pop('1')
-
-print(len(a))
 
